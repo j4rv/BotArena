6171f6302b157e80bf6bc6ca645d496211fce5af
Jarv <kira-8@hotmai.com>
2017-03-18 01:47:30 +0100

    Ordered the code in folders

---
+++ Assets/Code/Controllers/TurnController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BotArena
+{
+    public class TurnController : MonoBehaviour
+    {
+
+        [SerializeField]
+        private long timeSteps;
+        [SerializeField]
+        private int turnSteps;
+        [SerializeField]
+        private int currentTurn;
+
+        private static TurnController instance;
+
+        private static TurnController Get()
+        {
+            if (instance == null)
+                instance = (TurnController)FindObjectOfType(typeof(TurnController));
+            return instance;
+        }
+
+        void FixedUpdate()
+        {
+            if (IsTurnUpdate())
+            {
+                currentTurn++;
+            }
+            timeSteps++;
+        }
+
+        public static bool IsTurnUpdate()
+        {
+            bool res = false;
+
+            if(Get().turnSteps <= 0)
+            {
+                res = true;
+            } else {
+                res = (Get().timeSteps % Get().turnSteps) == 0;
+            }
+
+            return res;
+        }
+    }
+}
