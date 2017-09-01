472d8e4c4c167cd9bfaf3dd65df5913bd7056a46
Jarv <6jarv91@gmail.com>
2017-09-01 19:50:10 +0200

    Adding missing files from the last commit

---
+++ Assets/Code/Data/GameConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BotArena {
+    [System.Serializable]
+    internal class GameConfig {
+
+        private static GameConfig instance;
+
+        public static GameConfig Instance() {
+            if (instance == null) {
+                Debug.Log("Creating GameConfig instance");
+                instance = DataLoader.LoadGameConfig();
+            }
+
+            return instance;
+        }
+
+        public List<PlayerConfig> robots = new List<PlayerConfig>();
+        public int rounds = 10;
+        public float gameSpeed = 1;
+
+        [System.Serializable]
+        public class PlayerConfig
+        {
+            public string filename;
+            public string playerNickname;
+            public string robotToLoad = "(RANDOM)";
+        }
+
+        public string ToJson() {
+            return JsonUtility.ToJson(this);
+        }
+
+    }
+}
